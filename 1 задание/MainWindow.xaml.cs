f0a1f79a50532ab8b75712358c1e8629910da7e8
Ярослав <146277976+izumiprogs@users.noreply.github.com>
2023-12-08 09:57:40 +0300

    1 задание

---
+++ 1 задание/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _1_8_4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void BtnCancelClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void BtnOKClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int x = Convert.ToInt32(ListBoxData.Items[0]);
+                double s = 0;
+                int k = 0;
+                for (int i = 0; i <= x; ++i)
+                {
+                    int m = Convert.ToInt32(ListBoxData.Items[i]);
+                    if (x > 9 && x < 100)
+                    {
+                        s = s + x;
+                        k = k + 1;
+                    }
+                    if (k == 0) { TextBlockAnswer.Text = $"NO"; }
+
+                    else
+                    {
+                        s = s / k;
+                        TextBlockAnswer.Text = ($"{s}");
+                    }
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введены не корректные данные");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if ((String.IsNullOrEmpty(TbNumber.Text)))
+            {
+                return;
+            }
+            try
+            {
+                int xa = Convert.ToInt32(TbNumber.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введены не корректные данные");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            ListBoxData.Items.Add(TbNumber.Text);
+        }
+    }
+}
